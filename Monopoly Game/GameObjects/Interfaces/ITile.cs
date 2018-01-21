a69e0e77bbebd60efcf7bab9e4c76a3f3393c5e0
Mido Elgamal <mido.sameh.elgamal@gmail.com>
2018-01-21 11:35:09 -0500

    Addded ITile Comments

--- Monopoly Game/GameObjects/Interfaces/ITile.cs
+++ Monopoly Game/GameObjects/Interfaces/ITile.cs
@@ -9,15 +9,54 @@ namespace Monopoly_Game.GameObjects.Interfaces
 {
     public interface ITile
     {
+        /// <summary>
+        /// Gets the Label of the tile
+        /// </summary>
+        /// <returns>Tile label</returns>
         string GetLabel();
+        /// <summary>
+        /// Gets the price of tile
+        /// </summary>
+        /// <returns>Returns price if available</returns>
         int GetPrice();
+        /// <summary>
+        /// Links tile to owner
+        /// </summary>
+        /// <param name="owner">Player who bought the tile</param>
         void Buy(IPlayer owner);
+        /// <summary>
+        /// Unlinks tile from owner
+        /// </summary>
+        /// <param name="tile">Tile to unlink</param>
         void Sell(ITile tile);
+        /// <summary>
+        /// Preforms allowed game action
+        /// </summary>
         void Perfom();
-        void AddAction(IGameAction action); 
+        /// <summary>
+        /// Adds an action to the actions list
+        /// </summary>
+        /// <param name="action">Action to add</param>
+        void AddAction(IGameAction action);
+        /// <summary>
+        /// Gets all allowed actions
+        /// </summary>
+        /// <returns></returns>
         List<IGameAction> GetActions();
+        /// <summary>
+        /// Gets tile owner if tile is owned
+        /// </summary>
+        /// <returns>Onwer of tile</returns>
         IPlayer GetOwner();
+        /// <summary>
+        /// Checks if tile is owned
+        /// </summary>
+        /// <returns>Is Owned</returns>
         bool IsOwned();
+        /// <summary>
+        /// Gets rent of tile
+        /// </summary>
+        /// <returns>Rent if set</returns>
         int GetRent();
     }
 }
